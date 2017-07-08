0b24d3462c66391258401fbdf58930d1000da0c0
Dimitri Couwenberg <couwenberg@gmail.com>
2017-07-08 18:25:35 +1000

    created server.js

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+let app = express();
+app.set('port', (process.env.PORT || 3000))
+
+app.get('*', (req, res) => {
+
+   res.end('<p>hello from server</p>')
+})
+
+app.listen(app.get('port'))
+
+console.info('==> ✅  Server is listening in ' + process.env.NODE_ENV + ' mode')
+console.info('==> 🌎  Go to http://localhost:%s', app.get('port'))
